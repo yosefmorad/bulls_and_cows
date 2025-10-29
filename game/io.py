1b55867e10004c0fb520f29fe2f437c363716c7c
yosef morad <yos0556773004@gmail.com>
2025-10-29 10:27:29 +0200

    start

---
+++ game/io.py
@@ -0,0 +1,8 @@
+def io():
+    user_input = []
+    temp =input("enter number separated by space").split()
+    for  i in range(len(temp)):
+        user_input.append(int(i))
+
+
+    Treasure = {}
---
+++ game/secret.py
@@ -0,0 +1,15 @@
+import random
+def generate_secret(length:int=4 , unique_digits:bool = True ,allow_leading_zero:bool = False ,max_tries=None):
+    secret_num = []
+    counter = 0
+    while counter != length:
+        num = random.randrange(1 ,999)
+        if num not in secret_num:
+            secret_num.append(num)
+            counter +=1
+    print("max_tries:" , {max_tries})
+
+    return secret_num
+print(generate_secret())
+
+
